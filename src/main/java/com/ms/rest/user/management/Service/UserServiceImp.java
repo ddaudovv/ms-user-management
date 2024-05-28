183c078b520f57ac6852a6ba289db126d1c8e36a
Dzhan Daudov <djandaudovv@gmail.com>
2024-05-29 00:10:00 +0300

    Feat - Additional functionality for get all users, update and delete user by id.

--- src/main/java/com/ms/rest/user/management/Service/UserServiceImp.java
+++ src/main/java/com/ms/rest/user/management/Service/UserServiceImp.java
@@ -5,6 +5,7 @@ import com.ms.rest.user.management.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,5 +25,40 @@ public class UserServiceImp implements UserService {
     @Override
     public Optional<UserEntity> getUserById(Integer id) {
         return userRepository.findById(id);
+    }
+
+    @Override
+    public List<UserEntity> getAllUser() {
+        return  userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserEntity> updateUser(Integer id, UserEntity user) {
+        Optional<UserEntity> userToUpdate = getUserById(id);
+
+        if (userToUpdate.isPresent()) {
+            UserEntity updatedUser = userToUpdate.get();
+
+            updatedUser.setFirstName(user.getFirstName());
+            updatedUser.setLastName(user.getLastName());
+            updatedUser.setDateOfBirth(user.getDateOfBirth());
+            updatedUser.setPhoneNumber(user.getPhoneNumber());
+            updatedUser.setEmailAddress(user.getEmailAddress());
+            return Optional.of(userRepository.save(updatedUser));
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<UserEntity> deleteUser(Integer id) {
+        Optional<UserEntity> userToDelete = getUserById(id);
+
+        if (userToDelete.isPresent()) {
+            userRepository.deleteById(id);
+            return userToDelete;
+        }
+
+        return Optional.empty();
     }
 }
