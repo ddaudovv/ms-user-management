20333a7a4b03b6e1a99da25f2657dd83a324beb7
Dzhan Daudov <djandaudovv@gmail.com>
2024-05-29 22:53:27 +0300

    Update - Edited the Controller to correctly use the Service methods. JsonProperty is deleted from User Entity.

--- src/main/java/com/ms/rest/user/management/Controller/UserController.java
+++ src/main/java/com/ms/rest/user/management/Controller/UserController.java
@@ -1,8 +1,8 @@
 package com.ms.rest.user.management.Controller;
 
-import com.ms.rest.user.management.Entity.UserEntity;
+import com.ms.rest.user.management.DTO.UserRequestDTO;
+import com.ms.rest.user.management.DTO.UserResponseDTO;
 import com.ms.rest.user.management.Service.UserServiceImp;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,40 +17,47 @@ public class UserController {
     @Autowired
     private final UserServiceImp userService;
 
+
     public UserController(UserServiceImp userService) {
         this.userService = userService;
     }
 
+
     @PostMapping("/create")
-    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
-        UserEntity createdUser = userService.createUser(user);
-        return ResponseEntity.ok(createdUser);
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserRequestDTO userRequest) {
+        UserResponseDTO createdUser = userService.createUser(userRequest);
+
+        //return ResponseEntity.ok(createdUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
 
     @GetMapping("/{userId}")
-    public ResponseEntity<Optional<UserEntity>> getUser(@PathVariable Integer userId) {
+    public ResponseEntity<Optional<UserResponseDTO>> getUser(@PathVariable Integer userId) {
         try {
-            Optional<UserEntity> responseUser = userService.getUserById(userId);
+            Optional<UserResponseDTO> responseUser = userService.getUserById(userId);
 
             if (responseUser.isPresent()) {
                 return ResponseEntity.ok(responseUser);
             }
 
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Optional.empty());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Optional.empty());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
+
     @GetMapping("/all")
-    public List<UserEntity> getAllUsers() {
-        return userService.getAllUser();
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        List<UserResponseDTO> users = userService.getAllUser();
+        return ResponseEntity.ok(users);
     }
 
+
     @PutMapping("/update/{userId}")
-    public ResponseEntity<Optional<UserEntity>> updateUser(@PathVariable Integer userId, @RequestBody UserEntity newUser) {
-        Optional<UserEntity> updatedUser = userService.updateUser(userId, newUser);
+    public ResponseEntity<Optional<UserResponseDTO>> updateUser(@PathVariable Integer userId, @RequestBody UserRequestDTO newUser) {
+        Optional<UserResponseDTO> updatedUser = userService.updateUser(userId, newUser);
 
         if (updatedUser.isPresent()) {
             return ResponseEntity.ok(updatedUser);
@@ -59,10 +66,11 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
+
     @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<Optional<UserEntity>> deleteUser(@PathVariable Integer userId) {
+    public ResponseEntity<Optional<UserResponseDTO>> deleteUser(@PathVariable Integer userId) {
         try {
-            Optional<UserEntity> deletedUser = userService.deleteUser(userId);
+            Optional<UserResponseDTO> deletedUser = userService.deleteUser(userId);
 
             if (deletedUser.isPresent()) {
                 return ResponseEntity.ok(deletedUser);
--- src/main/java/com/ms/rest/user/management/Entity/UserEntity.java
+++ src/main/java/com/ms/rest/user/management/Entity/UserEntity.java
@@ -1,6 +1,5 @@
 package com.ms.rest.user.management.Entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
@@ -13,23 +12,18 @@ public class UserEntity {
     private int id;
 
     @Column(name = "first_name")
-    @JsonProperty("first_name")
     private String firstName;
 
     @Column(name = "last_name")
-    @JsonProperty("last_name")
     private String lastName;
 
     @Column(name = "date_of_birth")
-    @JsonProperty("date_of_birth")
     private LocalDate dateOfBirth;
 
     @Column(name = "phone_number")
-    @JsonProperty("phone_number")
     private String phoneNumber;
 
     @Column(name = "email_address")
-    @JsonProperty("email_address")
     private String emailAddress;
 
     // No Args Constructor
